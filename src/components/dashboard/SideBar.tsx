3f1c91b61eb6418b9e811b9d79840b20245c3c75
olayinka <adeleyeolayinka73@gmail.com>
2025-07-20 20:25:50 +0100

    feat(ui): implement sidebar with mobile and desktop responsiveness

--- src/components/dashboard/SideBar.tsx
+++ src/components/dashboard/SideBar.tsx
@@ -1,9 +1,21 @@
 import { FC } from 'react';
 
-interface Props {}
+interface Props {
+	setIsMenuOpen: (value: boolean) => void;
+	isMenuOpen: boolean;
+}
 
-const SideBar: FC<Props> = () => {
-	return <div>SideBar</div>;
+const SideBar: FC<Props> = ({ setIsMenuOpen, isMenuOpen }) => {
+	return (
+		<div className={`side-nav ${isMenuOpen && 'nav-active'}`}>
+			<div className="side-nav-header">
+				<img src="/images/logo.svg" alt="logo" className="logo" />
+			</div>
+			<div className="side-nav-menu">
+
+			</div>
+		</div>
+	)
 };
 
 export default SideBar;
